52ca0c7a7ceacf28952cf6ab74ac5d42fcd8781d
Cimpean Rares <rares_cimpean@yahoo.com>
2021-08-13 15:35:07 +0300

    final code cleanup and verification

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
 Route::get('/', [ProductController::class, 'show']);
 
 Route::get('/admin', function () {
